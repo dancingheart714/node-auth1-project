1a8cc1adfac8b694b734821a161f8f7be5a5b0e4
Leah King <dancingheart714@yahoo.com>
2021-05-17 16:18:30 -0400

    users work

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,16 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
+const router = require('express').Router();
+const Users = require('./users-model);
 
+const protected = (req, res, next) => {
+  if (req.session.user) {
+    next()
+  } else {
+    next({ 
+      message: 'You shall not pass!'
+    })
+  }
+}
 
 /**
   [GET] /api/users
@@ -24,5 +35,19 @@
   }
  */
 
+router.get('/', protected, (req, res, next) => {
+  Users.find()
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(next)
+})
 
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+    customMessage: "Something went wrong inside the users router"
+  });
+});
 // Don't forget to add the router to the `exports` object so it can be required in other modules
